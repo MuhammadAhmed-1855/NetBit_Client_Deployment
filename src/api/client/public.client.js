ba378f5a9a7cd2bceccaada1a41d81710f413fca
MuhammadAhmed-1855 <i201855@nu.edu.pk>
2024-01-02 01:07:07 +0500

    Backend Deployed on AWS

--- src/api/client/public.client.js
+++ src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://net-bit-server-deployment.vercel.app/api/v1/";
+const baseURL = "http://52.23.157.12/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
